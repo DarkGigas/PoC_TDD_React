deae7dcf28f2648ae37c35e55b74c519054efc9e
DarkGigas <darkgigas20@gmail.com>
2021-03-24 18:49:37 -0300

    test: adds a few tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,23 +5,30 @@ import App from "./App"
 Enzyme.configure({ adapter: new Adapter() })
 
 test("renders without crashing", () => {
-  const wrapper = shallow(<App/>)
+  const wrapper = shallow(<App />)
   expect(wrapper).toBeTruthy() // Checks if wrapper is not undefined/null
   const appComponent = wrapper.find("[data-test='app-component']")
   expect(appComponent.length).toBe(1)
 })
 
-// test("renders button", () => {
-// })
+test("renders button", () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy() // Checks if wrapper is not undefined/null
+  const button = wrapper.find("[data-test='increment-button']")
+  expect(button.length).toBe(1)
+})
 
-// test("renders counter display", () => {
-  
-// })
+test("renders counter display", () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy() // Checks if wrapper is not undefined/null
+  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  expect(counterDisplay.length).toBe(1)
+})
 
 // test("counter starts at 0", () => {
-  
+
 // })
 
 // test("button click updates counter", () => {
-  
+
 // })
