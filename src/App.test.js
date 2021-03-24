d7655166557afdade754c0ffbf4b3b363540ce5f
DarkGigas <darkgigas20@gmail.com>
2021-03-24 19:08:29 -0300

    refactor: removes some code smell

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,44 @@
-import Enzyme, { shallow } from 'enzyme'
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import App from "./App"
 
 Enzyme.configure({ adapter: new Adapter() })
 
+/**
+ * Factory function to create a ShallowWrapper for the App Component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />)
+
+/**
+ * Wrapper testing function that returns 
+ * an html Node or null if it does not find anything
+ * @function findByTestAttr
+ * @param {ShallowWrapper} wrapper 
+ * @param {string} value 
+ * @returns {ShallowWrapper | null}
+ */
+const findByTestAttr = (wrapper, value) => wrapper.find(`[data-test="${value}"]`)
+
 test("renders without crashing", () => {
-  const wrapper = shallow(<App />)
+  const wrapper = setup()
   expect(wrapper).toBeTruthy() // Checks if wrapper is not undefined/null
-  const appComponent = wrapper.find("[data-test='app-component']")
+  const appComponent = findByTestAttr(wrapper, "app-component")
   expect(appComponent.length).toBe(1)
 })
 
 test("renders button", () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper).toBeTruthy() // Checks if wrapper is not undefined/null
-  const button = wrapper.find("[data-test='increment-button']")
+  const wrapper = setup()
+  expect(wrapper).toBeTruthy()
+  const button = findByTestAttr(wrapper, "increment-button")
   expect(button.length).toBe(1)
 })
 
 test("renders counter display", () => {
-  const wrapper = shallow(<App />)
-  expect(wrapper).toBeTruthy() // Checks if wrapper is not undefined/null
-  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  const wrapper = setup()
+  expect(wrapper).toBeTruthy()
+  const counterDisplay = findByTestAttr(wrapper, "counter-display")
   expect(counterDisplay.length).toBe(1)
 })
 
