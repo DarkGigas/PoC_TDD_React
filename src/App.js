87a2baa0c01a13787e4adc17d749b6b8ed51bf7d
DarkGigas <darkgigas20@gmail.com>
2021-03-24 21:27:48 -0300

    test: adds test to track counter

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react"
 import "./App.css"
 
 function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <div className="App" data-test="app-component">
       <h1 data-test="counter-display">
-        The counter is currently
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
       </h1>
       <button data-test="increment-button">Increments Counter</button>
     </div>
